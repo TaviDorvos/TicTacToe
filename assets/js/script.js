6172d2dda0268369df0f39b9069da15a1b075ecb
TaviDorvos <octavian.dorvos@yahoo.com>
2021-02-15 11:06:32 +0200

    Added a restart button. Fixed a bug.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById("rr-btn").onclick = restartGame;
+
+    //Restart button - reload the page
+    function restartGame() {
+        location.reload();
+    }
+
     var boxes = document.querySelectorAll('.box');
     var currentPlayerDisplay = document.querySelector('#current-player');
 
@@ -24,35 +32,60 @@ document.addEventListener('DOMContentLoaded', () => {
             boxArray[index].innerHTML = textX;
             currentPlayer = 'playerO';
             currentPlayerDisplay.innerHTML = 'Player O';
+            document.getElementById(index).style.pointerEvents = "none";
         } else {
             boxArray[index].innerHTML = textO;
             currentPlayer = 'playerX';
             currentPlayerDisplay.innerHTML = 'Player X';
+            document.getElementById(index).style.pointerEvents = "none";
         }
-        // X WINS CASE
-        if (boxArray[0].innerHTML === textX && boxArray[1].innerHTML === textX && boxArray[2].innerHTML === textX ||
-            boxArray[3].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[5].innerHTML === textX ||
-            boxArray[6].innerHTML === textX && boxArray[7].innerHTML === textX && boxArray[8].innerHTML === textX ||
-            boxArray[0].innerHTML === textX && boxArray[3].innerHTML === textX && boxArray[6].innerHTML === textX ||
-            boxArray[1].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[7].innerHTML === textX ||
-            boxArray[2].innerHTML === textX && boxArray[5].innerHTML === textX && boxArray[8].innerHTML === textX ||
-            boxArray[0].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[8].innerHTML === textX ||
-            boxArray[2].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[6].innerHTML === textX) {
-            document.getElementById("x-win").classList.add("d-flex");
-            document.getElementById("x-win").classList.remove("d-none");
-            document.getElementById("container-id").style.opacity = "0.3";
-            //O WINS CASE
-        } else if (boxArray[0].innerHTML === textX && boxArray[1].innerHTML === textX && boxArray[2].innerHTML === textX ||
-            boxArray[3].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[5].innerHTML === textO ||
-            boxArray[6].innerHTML === textO && boxArray[7].innerHTML === textO && boxArray[8].innerHTML === textO ||
-            boxArray[0].innerHTML === textO && boxArray[3].innerHTML === textO && boxArray[6].innerHTML === textO ||
-            boxArray[1].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[7].innerHTML === textO ||
-            boxArray[2].innerHTML === textO && boxArray[5].innerHTML === textO && boxArray[8].innerHTML === textO ||
-            boxArray[0].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[8].innerHTML === textO ||
-            boxArray[2].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[6].innerHTML === textO) {
-            document.getElementById("o-win").classList.add("d-flex");
-            document.getElementById("o-win").classList.remove("d-none");
-            document.getElementById("container-id").style.opacity = "0.3";
+
+        //WINS CASES
+        // [0, 1, 2]
+        // [3, 4, 5]
+        // [6, 7, 8]
+        // [0, 3, 6]
+        // [1, 4, 7]
+        // [2, 5, 8]
+        // [0, 4, 8]
+        // [2, 4, 6]
+
+        for (let i = 0; i < boxArray.length; i += 3) {
+            // X WINS CASE
+            if (boxArray[i].innerHTML === textX && boxArray[i + 1].innerHTML === textX && boxArray[i + 2].innerHTML === textX) {
+                document.getElementById("x-win").classList.add("d-flex");
+                document.getElementById("x-win").classList.remove("d-none");
+                document.getElementById("gameboard").style.opacity = "0.3";
+                //O WINS CASE
+            } else if (boxArray[i].innerHTML === textO && boxArray[i + 1].innerHTML === textO && boxArray[i + 2].innerHTML === textO) {
+                document.getElementById("o-win").classList.add("d-flex");
+                document.getElementById("o-win").classList.remove("d-none");
+                document.getElementById("gameboard").style.opacity = "0.3";
+            }
         }
+        // if (boxArray[0].innerHTML === textX && boxArray[1].innerHTML === textX && boxArray[2].innerHTML === textX ||
+        //     boxArray[3].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[5].innerHTML === textX ||
+        //     boxArray[6].innerHTML === textX && boxArray[7].innerHTML === textX && boxArray[8].innerHTML === textX ||
+        //     boxArray[0].innerHTML === textX && boxArray[3].innerHTML === textX && boxArray[6].innerHTML === textX ||
+        //     boxArray[1].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[7].innerHTML === textX ||
+        //     boxArray[2].innerHTML === textX && boxArray[5].innerHTML === textX && boxArray[8].innerHTML === textX ||
+        //     boxArray[0].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[8].innerHTML === textX ||
+        //     boxArray[2].innerHTML === textX && boxArray[4].innerHTML === textX && boxArray[6].innerHTML === textX) {
+        //     document.getElementById("x-win").classList.add("d-flex");
+        //     document.getElementById("x-win").classList.remove("d-none");
+        //     document.getElementById("container-id").style.opacity = "0.3";
+        //     //O WINS CASE
+        // } else if (boxArray[0].innerHTML === textX && boxArray[1].innerHTML === textX && boxArray[2].innerHTML === textX ||
+        //     boxArray[3].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[5].innerHTML === textO ||
+        //     boxArray[6].innerHTML === textO && boxArray[7].innerHTML === textO && boxArray[8].innerHTML === textO ||
+        //     boxArray[0].innerHTML === textO && boxArray[3].innerHTML === textO && boxArray[6].innerHTML === textO ||
+        //     boxArray[1].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[7].innerHTML === textO ||
+        //     boxArray[2].innerHTML === textO && boxArray[5].innerHTML === textO && boxArray[8].innerHTML === textO ||
+        //     boxArray[0].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[8].innerHTML === textO ||
+        //     boxArray[2].innerHTML === textO && boxArray[4].innerHTML === textO && boxArray[6].innerHTML === textO) {
+        //     document.getElementById("o-win").classList.add("d-flex");
+        //     document.getElementById("o-win").classList.remove("d-none");
+        //     document.getElementById("container-id").style.opacity = "0.3";
+        // }
     }
 });
